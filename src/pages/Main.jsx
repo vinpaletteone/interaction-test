9b4a6d95c4d6d7e21658893f054b67fe513ad8d8
“vinpaletteone”  <binsuzy95@gmail.com>
2022-03-18 11:24:02 +0900

    Header 컴포넌트 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Header from '../components/Layout/Header'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <>
+        <Header />
+    </>
   )
 }
